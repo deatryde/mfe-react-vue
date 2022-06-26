0844d2079a747a40a668ce0779fed0a4caa951ec
Rahul Dogra <rahuldogra1998@gmail.com>
2022-06-26 22:45:43 +0530

    :rocket: Production webpack config

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const CommonConfig = require('./webpack.common');
+
+const deps = require('../package.json').dependencies;
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/maerketing/remoteEntry.js`,
+      },
+      shared: {
+        ...deps,
+      },
+    }),
+  ],
+};
+
+module.exports = merge(CommonConfig, prodConfig);
