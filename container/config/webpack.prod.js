ad9c638b1b3329398052d52702c3a124f2bf24a1
Rahul Dogra <rahuldogra1998@gmail.com>
2022-06-27 00:36:44 +0530

    :bug: Mfe deployment fix

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -10,6 +10,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publiPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
